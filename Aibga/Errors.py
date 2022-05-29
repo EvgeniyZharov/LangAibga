e273f6a39cfa09fbe9b606e97a1d9a7df32c702b
EvgeniyZharov <76780101+EvgeniyZharov@users.noreply.github.com>
2022-05-29 06:37:45 +0300

    Update Errors.py

--- Aibga/Errors.py
+++ Aibga/Errors.py
@@ -26,38 +26,34 @@ class FalseKod(Exception):
         return errorMessage(text)
 
 
-class NotSemicolon(Exception):
+class NotSymbol(Exception):
+
+    def __init__(self, data, line):
+        self.data = data
+        self.line = line
+
+    def __str__(self):
+        text = f'Не хватает символа "{self.data}" в {self.line} строке'
+        return errorMessage(text)
+
+
+class NotSemicolon(NotSymbol):
 
     def __init__(self, line):
-        self.line = line
-
-    def __str__(self):
-        text = f"Не хватает символа в {self.line} строке: ;"
-        return errorMessage(text)
+        data = ";"
+        super().__init__(data, line)
 
 
-class NotBracket(Exception):
+class NotBracket(NotSymbol):
 
     def __init__(self, data, line):
-        self.data = data
-        self.line = line
-
-    def __str__(self):
-        text = f"Не корректное количество символов " \
-               f"в {self.line} строке: {self.data}"
-        return errorMessage(text)
+        super().__init__(data, line)
 
 
-class NotFigureBracket(Exception):
+class NotFigureBracket(NotSymbol):
 
     def __init__(self, data, line):
-        self.data = data
-        self.line = line
-
-    def __str__(self):
-        text = f"Не корректное количество символов " \
-               f"в {self.line} строке: {self.data}"
-        return errorMessage(text)
+        super().__init__(data, line)
 
 
 class IntInStartLine(Exception):
